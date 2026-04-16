9a9991754b9833e8e977089c510b7f57d0b988d7
Kenny11423 <longvuvip10@gmail.com>
2026-04-17 02:13:44 +0700

    change swww to awww

--- config/hypr/hyprconfs/random_wallpaper.sh
+++ config/hypr/hyprconfs/random_wallpaper.sh
@@ -1,20 +1,32 @@
 #!/usr/bin/env bash
+
 set -e
 
-WALL_DIR="/home/kennysk/Wallpapers"
+WALL_DIR="$HOME/Wallpapers"
+CACHE_DIR="$HOME/.cache/awww"
 
-# đảm bảo swww đang chạy
-swww query >/dev/null 2>&1 || swww init
-sleep 0.5
+# đảm bảo cache tồn tại
+mkdir -p "$CACHE_DIR"
+
+# nếu daemon chết hoặc chưa chạy → restart sạch
+if ! pgrep -x awww-daemon >/dev/null; then
+  pkill awww-daemon 2>/dev/null || true
+  rm -rf "$CACHE_DIR"/*
+  
+  awww-daemon >/dev/null 2>&1 &
+  sleep 1
+fi
 
 # chọn ảnh random
-WALLPAPER=$(find "$WALL_DIR" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.webp" \) | shuf -n 1)
+WALLPAPER=$(find "$WALL_DIR" -type f \( \
+  -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.webp" -o -iname "*.gif" \
+\) | shuf -n 1)
 
-# nếu không có ảnh
+# nếu không có ảnh thì thoát
 [ -z "$WALLPAPER" ] && exit 0
 
-# set wallpaper
-swww img "$WALLPAPER" \
+# set wallpaper (fallback nếu transition lỗi)
+awww img "$WALLPAPER" \
   --transition-type grow \
-  --transition-duration 1
-
+  --transition-duration 1 \
+  || awww img "$WALLPAPER"
